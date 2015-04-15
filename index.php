867312e49089b83d6485171b7aad4d5154493f97
Sebastiaan Dirks <513811@lln.corlaercollege.nl>
2015-04-15 06:56:13 +0000

    Henk-Jan maak de site.

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Style.css -->
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
         
+        <!-- Javascript -->
+        <script type="text/javascript" src="js/java.css"></script>
     </head>
     
     <body>
