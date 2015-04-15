2085916fa972305d6339370023408a8541ac840f
Sebastiaan Dirks <513811@lln.corlaercollege.nl>
2015-04-15 07:04:51 +0000

    GoGo Henk-Jan

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Homepage - HJ</title>
+        <meta charset="utf-8">
+        
         <!-- Style.css -->
         <link rel="stylesheet" href="css/style.css" type="text/css" />
         
